732c35b69eef4410d227eef668f1b39fa156081d
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2020-10-05 02:11:18 -0500

    Starter code and assignment instructions.

---
+++ assets/scripts/main.js
@@ -0,0 +1,14 @@
+/* 
+  Add relevant functions in this file to do the following:
+  
+  - Load data from the remote server API
+  - Create a new DOM sub-tree of elements to display the data in a table
+  - Apply sorting of the data based on the user's selection
+  - Apply filtering of the data based on the user's selection
+  - Any supporting functions required by the above
+
+  Note: You may want to create a function that allows you to create
+  arbitrary HTML subtrees by specifying a top-level tag name and
+  any child DOM nodes as arguments, then use that to construct the
+  DOM tree for the data table.
+*/
